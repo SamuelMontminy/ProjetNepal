4ffbf6719e19e12b4f46f7b93d35c300cafa34df
Unknown <smontminysds@gmail.com>
2019-02-25 14:22:23 -0500

    Première version code MQTT
    Ce code est pour le plan B (avec Arduino MKR GSM 1400). Pour l'instant, le code fait juste envoyer une donnée (entrée analogique flotante) sur un broker générique de Eclipse (sans certificats). Il est possible de voir les données reçues sur Chirpers.com

---
+++ arduino_secrets.h
@@ -0,0 +1,9 @@
+#define SECRET_PINNUMBER     ""
+#define SECRET_GPRS_APN      "hologram" // replace your GPRS APN
+#define SECRET_GPRS_LOGIN    ""    // replace with your GPRS login
+#define SECRET_GPRS_PASSWORD "" // replace with your GPRS password
+
+#define BROKER "iot.eclipse.org"
+#define PORT "1883"
+#define TOPIC "hologram-projetnepal/to"
+#define PUBLISHTOPIC "hologram-projetnepal/from"
