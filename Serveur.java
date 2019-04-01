a48b0d6549f78828da21297d0e6f98a2152897c2
Unknown <smontminysds@gmail.com>
2019-04-01 09:38:44 -0400

    V1.1
    Première ébauche du code qui va permettre d'enregistrer les trames sur une longue période de temps avant de les envoyer

--- Serveur.java
+++ Serveur.java
@@ -5,7 +5,8 @@
  * @brief  Code qui permet de recevoir des données envoyées par le client tcp/ip pour ensuite les envoyer sur le dashboard Hologram par LTE
  *         Le code doit être compilé avec /javac Serveur.java et doit être lancé avec /java Serveur
  *
- * @version 1.0 : Premiere version
+ * @version 1.0 : Première version
+ * @version 1.1 : Deuxième version du fichier, enregistre les fichiers dans le système avant de les envoyer une fois par jour pour sauver du courant.
  * Environnement de developpement: GitKraken / Notepad++
  * Compilateur: javac (Java version 1.8)
  * Matériel: Raspberry Pi Zero W, Module LTE Hologram (+ carte SIM)
@@ -13,6 +14,7 @@
 
 import java.net.*;
 import java.io.*;
+import java.util.regex.*;
 
 public class Serveur implements Runnable
 {
@@ -20,6 +22,10 @@ public class Serveur implements Runnable
     int m_nPort = 2228;                                             //Numéro du port utilise par le miniserveur (doit être entré comme argument lorsque les codes clients sont lancés)
     ServerSocket m_ssServeur;                                       //Reference vers l'objet ServerSocket
     Thread m_tService;                                              //Reference vers l'objet Thread
+
+    String pattern = "^\"(\\w{2}),(\\d+\\.?(?:\\d+)?),(\\d+\\.?(?:\\d+)?),(\\d+\\.?(?:\\d+)?),(\\d+\\.?(?:\\d+)?)\"$";
+
+    public EnvoieInformations m_objInformations;                    //Référence du thread qui sert à envoyer les informations
     
     public Serveur()
     {		
@@ -29,6 +35,8 @@ public class Serveur implements Runnable
                                                                             //Le miniserveur a un tampon mémoire de 1000 octets (NB_OCTETS)
             m_tService = new Thread(this);                                  //Creation et démarrage de la tâche d'écoute du miniserveur sur le port 2228
             m_tService.start();
+
+            m_objInformations = new EnvoieInformations(this);               //Démarre le thread qui sert à envoyer les informations
         }
         
         catch (IOException e)
@@ -82,7 +90,69 @@ public class Serveur implements Runnable
 			{
 				System.out.println(Informations + " -> à été reçu d'un client");
 				
-                /*//Ce bloc permet de d'activer l'alimentation sur les ports USB                  //<- DÉBUT DU BLOC
+                Pattern r = Pattern.compile(pattern);
+                Matcher m = r.matcher(Informations);
+
+                if (m.find( ))
+                {
+                    System.out.println("Found value: " + m.group(0));
+                    System.out.println("Found value: " + m.group(1));
+                    System.out.println("Found value: " + m.group(2));
+                    System.out.println("Found value: " + m.group(3));
+                    System.out.println("Found value: " + m.group(4));
+                }
+                
+                else
+                {
+                    System.out.println("NO MATCH");
+                }
+
+                //MODIFIER INFORMATIONS POUR METTRE LA DATE/HEURE DEDANS
+                //ENREGISTRER INFORMATIONS DANS UN FICHIER .TXT
+			}
+
+			catch (Exception e)
+			{
+				System.out.println(e.toString());                               //Affiche l'erreur survenue en Java
+			}
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        new Serveur();                                                    		//Appelle le constructeur de la classe ServeurSocket (pas d'arguments)
+    }
+}
+
+class EnvoieInformations implements Runnable
+{
+    Thread m_Thread;
+    private Serveur m_Parent;
+    String Donnee = "";
+
+    public EnvoieInformations(Serveur Parent)
+    {
+        try
+        {
+            m_Parent = Parent;
+
+            m_Thread = new Thread(this);
+            m_Thread.start();
+        }
+
+        catch (Exception e)
+        {
+            System.out.println(e.toString());
+        }
+    }
+
+    public void run()
+    {
+        while (true)
+        {
+            try
+            {
+                //Ce bloc permet de d'activer l'alimentation sur les ports USB                  //<- DÉBUT DU BLOC
                 String s1 = "echo '1-1' |sudo tee /sys/bus/usb/drivers/usb/bind";    			//Commande bash a etre executee
 				String[] sCmd1 = {"/bin/bash", "-c", s1};             			                //Specifie que l'interpreteur de commandes est BASH. Le "-c" indique que la commande a executer suit
 
@@ -100,17 +170,19 @@ public class Serveur implements Runnable
 				Thread.sleep(100);      										                //Delai pour laisser le temps au kernel d'agir
                                                                                                 //<-FIN DU BLOC
 																								
-				Thread.sleep(30000);               //Délai de 30 secondes pour laisser le temps au modem d'avoir un signal LTE
+                //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+				Thread.sleep(60000);               //Délai de 30 secondes pour laisser le temps au modem d'avoir un signal LTE
+                //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 				
 				//Bloc qui sert à faire un test de connection avant d'envoyer des données     	//<- DÉBUT DU BLOC
-				System.out.println(Informations + " -> sera envoyé à Hologram");
-				String s4 = "sudo hologram network connect";    			    	//Commande bash a etre executee
-				String[] sCmd4 = {"/bin/bash", "-c", s4};             			    //Specifie que l'interpreteur de commandes est BASH. Le "-c" indique que la commande a executer suit
+				System.out.println(Donnee + " -> sera envoyé à Hologram");
+				String s4 = "sudo hologram network connect";    			    	            //Commande bash a etre executee
+				String[] sCmd4 = {"/bin/bash", "-c", s4};             			                //Specifie que l'interpreteur de commandes est BASH. Le "-c" indique que la commande a executer suit
 
-				System.out.println(sCmd4[0] + " " + sCmd4[1] + " " + sCmd4[2]);     //Affiche la commande a executer dans la console Java
-				Process p4 = Runtime.getRuntime().exec(sCmd4);        			    //Execute la commande par le systeme Linux (le programme Java doit etre demarré par le root pour les acces aux GPIO)
+				System.out.println(sCmd4[0] + " " + sCmd4[1] + " " + sCmd4[2]);                 //Affiche la commande a executer dans la console Java
+				Process p4 = Runtime.getRuntime().exec(sCmd4);        			                //Execute la commande par le systeme Linux (le programme Java doit etre demarré par le root pour les acces aux GPIO)
 
-				if (p4.getErrorStream().available() > 0)        					//Verification s'il y a une erreur d'execution par l'interpreteur de commandes BASH
+				if (p4.getErrorStream().available() > 0)        					            //Verification s'il y a une erreur d'execution par l'interpreteur de commandes BASH
 				{
 					//Affiche l'erreur survenue
 					BufferedReader brCommand4 = new BufferedReader(new InputStreamReader(p4.getErrorStream()));
@@ -118,20 +190,22 @@ public class Serveur implements Runnable
 					brCommand4.close();
 				}
 
-				Thread.sleep(100);      										    //Delai pour laisser le temps au kernel d'agir
-				                                                                    //<- FIN DU BLOC
+				Thread.sleep(100);      										                //Delai pour laisser le temps au kernel d'agir
+				                                                                                //<- FIN DU BLOC
 
-                Thread.sleep(60000);               //Délai de 60 secondes pour laisser le temps au modem d'avoir un signal LTE*/
+                //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                Thread.sleep(30000);               //Délai de 60 secondes pour laisser le temps au modem d'avoir un signal LTE
+                //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-				//Mettre en commentaire le bloc pour ne pas envoyer à Hologram      //<- DÉBUT DU BLOC
-				System.out.println(Informations + " -> sera envoyé à Hologram");
-				String s2 = "sudo hologram send " + Informations;    			    //Commande bash a etre executee
-				String[] sCmd2 = {"/bin/bash", "-c", s2};             			    //Specifie que l'interpreteur de commandes est BASH. Le "-c" indique que la commande a executer suit
+				//Ce bloc éxécute la commande qui envoie les informations à Hologram            //<- DÉBUT DU BLOC
+				System.out.println(Donnee + " -> sera envoyé à Hologram");
+				String s2 = "sudo hologram send " + Donnee;    			                //Commande bash a etre executee
+				String[] sCmd2 = {"/bin/bash", "-c", s2};             			                //Specifie que l'interpreteur de commandes est BASH. Le "-c" indique que la commande a executer suit
 
-				System.out.println(sCmd2[0] + " " + sCmd2[1] + " " + sCmd2[2]);     //Affiche la commande a executer dans la console Java
-				Process p2 = Runtime.getRuntime().exec(sCmd2);        			    //Execute la commande par le systeme Linux (le programme Java doit etre demarré par le root pour les acces aux GPIO)
+				System.out.println(sCmd2[0] + " " + sCmd2[1] + " " + sCmd2[2]);                 //Affiche la commande a executer dans la console Java
+				Process p2 = Runtime.getRuntime().exec(sCmd2);        			                //Execute la commande par le systeme Linux (le programme Java doit etre demarré par le root pour les acces aux GPIO)
 
-				if (p2.getErrorStream().available() > 0)        					//Verification s'il y a une erreur d'execution par l'interpreteur de commandes BASH
+				if (p2.getErrorStream().available() > 0)        					            //Verification s'il y a une erreur d'execution par l'interpreteur de commandes BASH
 				{
 					//Affiche l'erreur survenue
 					BufferedReader brCommand2 = new BufferedReader(new InputStreamReader(p2.getErrorStream()));
@@ -139,11 +213,12 @@ public class Serveur implements Runnable
 					brCommand2.close();
 				}
 
-				Thread.sleep(100);      										    //Delai pour laisser le temps au kernel d'agir
-				System.out.println(Informations + " -> à été envoyé à Hologram");
-				                                                                    //<- FIN DU BLOC
-																					
-				/*Thread.sleep(30000);               //Délai de 30 secondes pour laisser le temps d'envoyer la donnée
+				Thread.sleep(100);      										                //Delai pour laisser le temps au kernel d'agir
+				System.out.println(Donnee + " -> à été envoyé à Hologram");               //<- FIN DU BLOC   
+
+                //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-								
+				Thread.sleep(30000);               //Délai de 30 secondes pour laisser le temps d'envoyer la donnée
+                //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
                 //Ce bloc permet de de désactiver l'alimentation sur les ports USB                  //<- DÉBUT DU BLOC
                 String s3 = "echo '1-1' |sudo tee /sys/bus/usb/drivers/usb/unbind";    			    //Commande bash a etre executee
@@ -161,18 +236,13 @@ public class Serveur implements Runnable
 				}
 
 				Thread.sleep(100);      										                    //Delai pour laisser le temps au kernel d'agir
-                                                                                                    //FIN DU BLOC*/
-			}
+                                                                                                    //<- FIN DU BLOC
+            }
 
-			catch (Exception e)
-			{
-				System.out.println(e.toString());                               //Affiche l'erreur survenue en Java
-			}
+            catch (Exception e)
+            {
+                System.out.println(e.toString());
+            }
         }
-    }
-    
-    public static void main(String[] args)
-    {
-        new Serveur();                                                    		//Appelle le constructeur de la classe ServeurSocket (pas d'arguments)
     }
 }
